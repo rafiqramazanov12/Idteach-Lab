2683df48a67d5de77698941c9bdec66a00fd6ad9
rafiq ramazanov <rafiq12.dev@gmail.com>
2025-02-22 09:33:57 +0400

    homework done

--- task-9/main.js
+++ task-9/main.js
@@ -15,54 +15,54 @@
 
                     // task-2
 
-// const input = document.querySelector("input"),
-//   guess = document.querySelector(".wrapper_header_guess"),
-//   checkButton = document.querySelector(".input_field_btn"),
-//   remainChances = document.querySelector(".chances");
-// input.focus();
+const input = document.querySelector("input"),
+  guess = document.querySelector(".wrapper_header_guess"),
+  checkButton = document.querySelector(".input_field_btn"),
+  remainChances = document.querySelector(".chances");
+input.focus();
 
-// const resetGame = () => {
+const resetGame = () => {
 
-//   randomNum = Math.floor(Math.random() * 100); 
-//   chance = 10; 
-//   input.disabled = false; 
-//   remainChances.textContent = chance; 
-//   guess.textContent = ""; 
-//   guess.style.color = "#333"; 
-//   input.value = ""; 
-//   checkButton.textContent = "Check"; 
-// };
+  randomNum = Math.floor(Math.random() * 100); 
+  chance = 10; 
+  input.disabled = false; 
+  remainChances.textContent = chance; 
+  guess.textContent = ""; 
+  guess.style.color = "#333"; 
+  input.value = ""; 
+  checkButton.textContent = "Check"; 
+};
 
-// let randomNum = Math.floor(Math.random() * 100);
-// console.log(randomNum)
-// let chance = 10;
-// checkButton.addEventListener("click", () => {
-//   if (input.disabled) {
-//     resetGame();
-//     return;
-//   }
-//   chance--; 
-//   let inputValue = input.value;
-//   if
-//    (inputValue == randomNum) { 
-//     [guess.textContent, input.disabled] = ["Contgrats! You found the number.", true];
-//     [checkButton.textContent, guess.style.color] = ["Replay", "#27ae60"];
-//   }
-//    else if (inputValue > randomNum && inputValue < 100) { 
-//     [guess.textContent, remainChances.textContent] = ["Your guess is high", chance];
-//     guess.style.color = "#333";
+let randomNum = Math.floor(Math.random() * 100);
+console.log(randomNum)
+let chance = 10;
+checkButton.addEventListener("click", () => {
+  if (input.disabled) {
+    resetGame();
+    return;
+  }
+  chance--; 
+  let inputValue = input.value;
+  if
+   (inputValue == randomNum) { 
+    [guess.textContent, input.disabled] = ["Contgrats! You found the number.", true];
+    [checkButton.textContent, guess.style.color] = ["Replay", "#27ae60"];
+  }
+   else if (inputValue > randomNum && inputValue < 100) { 
+    [guess.textContent, remainChances.textContent] = ["Your guess is high", chance];
+    guess.style.color = "#333";
 
-//   }
-//    else if (inputValue < randomNum && inputValue > 0) { 
-//     [guess.textContent, remainChances.textContent] = ["Your guess is low", chance];
-//     guess.style.color = "#333";
-//   }
-//    else { 
-//     [guess.textContent, remainChances.textContent] = ["Your number is invalid", chance];
-//     guess.style.color = "#e74c3c";
-//   }
-//   if (chance == 0) { 
-//     [checkButton.textContent, input.disabled, inputValue] = ["Replay", true, ""];
-//     [guess.textContent, guess.style.color] = ["You lost the game", "#e74c3c"];
-//   }
-// });
+  }
+   else if (inputValue < randomNum && inputValue > 0) { 
+    [guess.textContent, remainChances.textContent] = ["Your guess is low", chance];
+    guess.style.color = "#333";
+  }
+   else { 
+    [guess.textContent, remainChances.textContent] = ["Your number is invalid", chance];
+    guess.style.color = "#e74c3c";
+  }
+  if (chance == 0) { 
+    [checkButton.textContent, input.disabled, inputValue] = ["Replay", true, ""];
+    [guess.textContent, guess.style.color] = ["You lost the game", "#e74c3c"];
+  }
+});
